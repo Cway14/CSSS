25714b9b4c3b52f80e3376a631c083c6b8a5c14f
Cam Way <cway140@gmail.com>
2020-11-07 11:59:45 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
-
+import './css/tailwind.output.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-blue-500">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
