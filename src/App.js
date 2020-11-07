c11ecfe53c8c59e598b1fdc81de08aa031b9d9c0
Cam Way <cway140@gmail.com>
2020-11-07 14:00:28 -0800

    added sidbar, expanded on course component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
 
+import React, {useState} from 'react';
 import './css/tailwind.output.css'
 import Course from './course';
 
 function App() {
   const courses  = [
   {
-    id: 120,
-    title: "Test Course",
-    description: "",
-    prerequisites: [],
+    title: "CMPT 276",
+    description: "asdgadbg",
+    prerequisites: ["CMPT 120", "CMPT 225"],
     rating: "easy",
     completed: false,
     enabled: true, //enabled if prereq's met
-  },
-  {
-    title: "Test Course",
-    description: "",
-    prerequisites: [120, 125],
-    rating: "easy",
-    completed: false,
-    enabled: true, //enabled if prereq's met
-  },
-]
+    },
+    {
+      title: "CMPT 225",
+      description: "asdgadbg",
+      prerequisites: ["CMPT 120", "CMPT 225"],
+      rating: "easy",
+      completed: false,
+      enabled: true, //enabled if prereq's met
+      },
+  ]
+  const [selectedCourse, setCourse] = useState(courses[0]);
+
   return (
-    <div className="App"> 
-      {courses.map((course) => (
-        <Course title="my title"></Course>
-      ))}
+    <div className="flex"> 
+      <div className="w-1/2">
+        {courses.map((course) => (
+            <Course {...course} focusedCourse={setCourse}></Course>
+          ))}
+      </div>
+      <div className="w-1/2 m-2 p-4 rounded-lg h-screen bg-gray-100 text-center">
+        <h1 className="text-2xl font-bold w-full border-b">{selectedCourse.title}</h1>
+        <p className="mt-4">{selectedCourse.description}</p>
+      </div>
     </div>
   );
 }
--- src/course.js
+++ src/course.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-function course(info){
+function Course(info){
+    const [isChecked, toggleChecked] = useState(false);
+    console.log(info);
     return(
-        <div className="m-2 p-4 bg-gray-200 rounded-lg" >
-            {/*Course Title */}
-            <h1>{info.title}</h1>
+        <div className="flex-col m-2 p-4 bg-gray-200 rounded-lg" onClick={() => info.focusedCourse(info)}>
+            <div className="flex flex-row items-center">
+                <svg className={`${isChecked ? 'stroke-current text-red-700' : 'stroke-current text-green-700'} h-8`} 
+                    onClick={()=> toggleChecked(!isChecked)}
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                {/*Course Title */}
+                <h1 className="m-2">{info.title}</h1>
+            </div>
         </div>
     )
 }
 
-export default course;
+export default Course;
